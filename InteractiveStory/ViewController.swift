771e3db2a0dd006986441097c1322b73528596ab
joanitac <joanita@statuscreative.co.uk>
2016-10-09 00:18:42 +0100

    InteractiveStory model complete

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Too crumblesome
+        let story = Page(story: TouchDown)
+        story.firstChoice = (title: "Some Title", page: Page(story: Droid))
+        */
+    
     }
 
     override func didReceiveMemoryWarning() {
